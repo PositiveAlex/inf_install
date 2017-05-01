afde9e2708e269ea3298a79d3960e49c91753748
Iljushkin, Alexander <Alexander.Iljushkin@billing.ru>
2017-05-01 13:53:19 +0300

    installing preparation packages

--- install_docker.py
+++ install_docker.py
@@ -2,5 +2,19 @@
 
 """Basic environment for my personal cloud server."""
 
+import subprocess
+
+def install_docker():
+    """Install docker into a server."""
+
+    cmd = "apt-get install apt-transport-https \
+                           ca-certificates \
+                           curl \
+                           software-properties-common"
+
+    retcode = subprocess.call(cmd, shell=True)
+    if retcode:
+        print "retcode {}".format(retcode)
+
 def __main__():
-    pass
+    install_docker()
