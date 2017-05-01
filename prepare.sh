d165b329379a435f97eaf48bda3fd0dd0b7d62f1
Iljushkin, Alexander <Alexander.Iljushkin@billing.ru>
2017-05-01 14:00:43 +0300

    prepare sh script

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+apt-get install -y python \
+                   python-pip \
+                   apt-transport-https \
+                   ca-certificates \
+                   curl \
+                   software-properties-common
