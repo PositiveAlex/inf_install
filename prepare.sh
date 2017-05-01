8316f69126e12116df320c8ad5042ad64b2eb289
Iljushkin, Alexander <Alexander.Iljushkin@billing.ru>
2017-05-01 22:09:16 +0300

    mail accounts for different folder

--- prepare.sh
+++ prepare.sh
@@ -34,7 +34,7 @@ docker run --rm \
   -e MAIL_USER=$MAIL_USER \
   -e MAIL_PASS=$MAIL_PASS \
   -ti tvial/docker-mailserver:latest \
-  /bin/sh -c 'echo "$MAIL_USER|$(doveadm pw -s SHA512-CRYPT -u $MAIL_USER -p $MAIL_PASS)"' > config/postfix-accounts.cf
+  /bin/sh -c 'echo "$MAIL_USER|$(doveadm pw -s SHA512-CRYPT -u $MAIL_USER -p $MAIL_PASS)"' > ./docker-mailserver/config/postfix-accounts.cf
 
 export COMPOSE_FILE=./docker-mailserver/docker-compose.yml
 
